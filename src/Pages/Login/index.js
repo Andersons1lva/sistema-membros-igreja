59fd10ac17da85e38f5e3a4157928b2c9d95050a
Anderson <Andersons1lva@yahoo.com.br>
2024-03-01 15:10:33 -0300

    Criação do index.js e styles.js do Login, criação também do global.js (styles global).

---
+++ src/Pages/Login/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container,Form } from "./styles";
+
+const Login = () => {
+    return ( 
+        <Container>
+            <Form>
+                <h1>Faça seu Login</h1>
+                <div>
+                    <p>Não possui conta?</p>
+                    <a>Cadastrar</a>
+                </div>
+            </Form>
+        </Container>
+     )
+}
+ 
+export default Login;
